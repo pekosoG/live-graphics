fa389c6c43c2a5dcaac6e427dd5644c17e1ca940
Israel Garcia <igarcia@itexico.net>
2018-06-29 19:43:23 -0500

    change on the render interval

--- live-graphics-vanilla/main.js
+++ live-graphics-vanilla/main.js
@@ -2,24 +2,24 @@ let zoom=.1
 let direction=.1;
 
 let canvas = document.getElementById('el-canvas');
-//canvas.width=window.innerWidth/2;
-//canvas.height=window.innerHeight/2;
+//canvas.width=window.innerWidth;
+//canvas.height=window.innerHeight;
 let ctx=canvas.getContext('2d');
 
-window.requestAnimationFrame(function loop(){
+function loop(){
     zoom+=.01;
     if(direction==.1 & zoom>=10)
         direction=-.1;
-    if(direction==-.1 & zoom<=.11)
+    if(direction==-.1 & zoom<=.1)
         direction=.1
     zoom+=direction;
-    console.log(zoom,direction);
     for(let y=0; y<canvas.height; y++){
         for(let x=0; x< canvas.width; x++){
             let value = Math.floor( x/zoom * y/zoom) % 2;
             ctx.fillStyle= value? 'orange' :'red';
             ctx.fillRect(x,y,1,1);
-        }
     }
-    window.requestAnimationFrame(loop);
-});
+ }
+}
+
+setInterval(()=>{window.requestAnimationFrame(loop)},100);
